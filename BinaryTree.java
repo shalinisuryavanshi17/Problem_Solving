273c66730a277b173fad7fd21d1d5444169f2d39
shalinisuryavanshi17 <gajbinkar.shalini0@gmail.com>
2021-05-29 18:31:28 +0530

    binary tree nodes insertion added

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,89 +1,63 @@
+class Tree{
+    private class Node{
+        private int data;
+        private Node leftChild;
+        private Node rightChild;
 
-/*
+        Node(int data){
+            this.data=data;
+        }
+    }
 
-Given an integer array element, construct a binary search tree(BST), and print the level order.
-Complete the given code : 
+    Node root;
+    //insert
+    public void insert(int data){
+        Node node = new Node(data);
+        if(root == null){
+            root = node;
+        }
+        else {
+            Node current = root;
+            while (true){
+                if(data < current.data){
+                    if(current.leftChild==null){
+                        current.leftChild=node;
+                        break;
+                    }
+                   current = current.leftChild;
+                }
+                else {
+                    if(current.rightChild==null){
+                        current.rightChild=node;
+                        break;
+                    }
+                    current = current.rightChild;
+                }
+            }
+        }
+    }
 
-	
-Example:
-Input: [7,4,15,1,5,3,12,6]
-Output: 7,4,15,1,5,12,3,6
 
+    public void print(){
+        print("",root,false);
+    }
 
-*/
-import java.util.*;
-
-class Tree {
-	private class Node {
-		private int data;
-		private Node leftChild;
-		private Node rightChild;
-
-		Node(int data) {
-			this.data = data;
-		}
-
-	}
-
-	private Node root;
-
-	public void insert(int value) {
-		// Write your code
-		Node node = new Node(value);
-		if (root == null) {
-			
-			root=node;
-			return;
-		}
-		Node curr = root;
-		while (true) {
-			if (value > curr.data) {
-				if (curr.rightChild != null) {
-					curr = curr.rightChild;
-					break;
-				}
-				else{
-					curr.rightChild= node;
-					
-	
-				}
-				
-			} else {
-				if (curr.leftChild != null) {
-					curr = curr.leftChild;
-				}
-				else{
-					curr.leftChild = node;
-					break;
-				}
-				
-			}
-		}
-	}
-
-	public void printLevelOrder() {
-		// Write your code
-		Node current = root;
-		while (current.leftChild != null || current.rightChild != null) {
-			System.out.print( current.rightChild.data);
-		}
-	}
-
+    private void print(String prefix, Node node, boolean isLeft){
+        if(node!=null){
+            System.out.println(prefix + (isLeft?"(L) ":"(R) ")+node.data);
+            print(prefix ,node.leftChild,true);
+            print(prefix ,node.rightChild,false);
+        }
+    }
 }
 
-public class BinaryTree {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Tree t = new Tree();
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		int i;
-		for (i = 0; i < arr.length; i++) {
-			t.insert(sc.nextInt());
-		}
-		t.printLevelOrder();
-		// Use Scanner class
-		// Write your code
-		// user t.insert and t.printLevelOrder methods
-	}
+public class BinaryTree{
+    public static void main(String[] args) throws Exception {
+       Tree tree = new Tree();
+       tree.insert(12);
+       tree.insert(15);
+       tree.insert(10);
+       tree.print();
+
+    }
 }
