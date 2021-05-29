339b6dc6a66553c8909e71e6cd67e9e5673eec36
shalinisuryavanshi17 <gajbinkar.shalini0@gmail.com>
2021-05-29 19:54:39 +0530

    code changes done

--- SortLinkedList.java
+++ SortLinkedList.java
@@ -24,7 +24,6 @@ public class SortLinkedList
     public Node head = null;  
     public Node tail = null;  
     PriorityQueue<Integer> min = new PriorityQueue<>(new Comparator<Integer>(){
-        @override
         public int compare(Integer a,Integer b)
         {
             return Integer.compare(a,b);
